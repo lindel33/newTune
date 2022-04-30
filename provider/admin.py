1df2154a7875d261479bfbf9f16c2f58a85398c4
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-04-30 20:06:21 +0300

    Update admin.py

--- provider/admin.py
+++ provider/admin.py
@@ -5,7 +5,7 @@ from .models import ProviderProduct
 @admin.register(ProviderProduct)
 class ProviderProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'author', 'provider_device']
-    exclude = ('booking', 'count', 'moderation', 'up_price', 'day_next_publish', 'name_tmp', 'device_provider', 'provider_device ')
+    exclude = ('booking', 'count', 'moderation', 'up_price', 'day_next_publish', 'name_tmp', 'device_provider', 'provider_device', 'author')
     
     def save_model(self, request, obj, form, change):
         if not obj.pk:
