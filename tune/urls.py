53f7a95681fb5f3fa541311a21abf31b2d1f614b
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-05-09 09:24:04 +0300

    Update urls.py

--- tune/urls.py
+++ tune/urls.py
@@ -2,6 +2,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.admin import AdminSite
+from django.http import HttpResponse
+
+
+class MyAdminSite(AdminSite):
+
+     def get_urls(self):
+         from django.urls import path
+         urls = super().get_urls()
+         urls += [
+             path('my_view/', self.admin_view(self.my_view))
+         ]
+         return urls
+
+     def my_view(self, request):
+         return HttpResponse("Hello!")
+
+admin_site = MyAdminSite()
 
 
 urlpatterns = [
