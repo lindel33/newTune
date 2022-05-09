521b2f6b76981dcbce70460bffb032c32a9f2b5d
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-05-09 18:20:10 +0300

    Update urls.py

--- tune/urls.py
+++ tune/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     
     path('api/', include('tune_admin.urls')),
     path('csv_check/', include('cost_models.urls')),
-     path('', admin.site.urls),
+     
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [path('', admin.site.urls)]
