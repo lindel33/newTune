b8dfed9cc832bf778fc075a8158464e64650076f
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-05-12 21:32:02 +0300

    Update admin.py

--- trade_in/admin.py
+++ trade_in/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TradeInStepModel, VariableFoeStepModel, \
+    TradeInSeriesModel, \
+    TradeInDevicesModel, \
+    UserStepModel, \
+    TelegramUserModel
+
+
+@admin.register(TelegramUserModel)
+class TelegramUserModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserStepModel)
+class UserStepModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TradeInSeriesModel)
+class TradeInSeriesModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TradeInDevicesModel)
+class TradeInDevicesModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TradeInStepModel)
+class TradeInStepModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(VariableFoeStepModel)
+class VariableFoeStepModelAdmin(admin.ModelAdmin):
+    pass
