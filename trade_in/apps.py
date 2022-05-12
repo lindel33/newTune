85bec2a4ac2df18ceab873b62a9b5619c2677ad4
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-05-12 21:33:18 +0300

    Update apps.py

--- trade_in/apps.py
+++ trade_in/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TradeInConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trade_in'
+    verbose_name = 'Меню Trade-in'
