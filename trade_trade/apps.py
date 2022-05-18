78466631d4d3bec3981af211749166ddaa5633ec
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-05-18 20:24:10 +0300

    Update apps.py

--- trade_trade/apps.py
+++ trade_trade/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TradeTradeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trade_trade'
+    verbose_name = 'Раздел обменки'
