413778d6a01e6b7157b3d9c498785cf07918f9f8
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-06-14 03:27:43 +0300

    Update admin.py

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+
+class UserInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Доп. информация'
+
+
+# Определяем новый класс настроек для модели User
+class MyUserAdmin(UserAdmin):
+    inlines = (UserInline,)
+
+
+# Перерегистрируем модель User
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
