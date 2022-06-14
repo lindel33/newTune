3b241e7b9802202008acb1ec9ef066b1a814773a
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-06-14 03:27:57 +0300

    Update models.py

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from tune_admin.models import RegionUserModel
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                verbose_name='Пользователь')
+    region = models.ForeignKey(RegionUserModel,
+                               on_delete=models.CASCADE,
+                               verbose_name='Регион',
+                               default=1,
+                               null=False,
+                               blank=False)
+
+    def __unicode__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'Настройки'
+        verbose_name_plural = 'Настройки'
