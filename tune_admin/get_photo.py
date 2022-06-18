d01b48e5602b9e8f5855be50c9b31fbfcd75b652
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-06-18 14:02:12 +0300

    Create get_photo.py

---
+++ tune_admin/get_photo.py
@@ -0,0 +1,22 @@
+def get_photo(series):
+    name = series.replace(' ', '').lower()
+
+    path_to_iphone = '/home/apple/code/project1/tune/tune_admin/ac_photo/'
+    if 'iphone' in name:
+      if 'iphone13mini' in name:
+          return path_to_iphone + 'iphone/iphone_13_mini.jpg'
+
+      if 'iphone13' in name and 'mini' not in name:
+          return path_to_iphone + 'iphone/iphone_13.jpg'
+
+      if 'iphone12mini' in name:
+          return path_to_iphone + 'iphone/iphone_12.jpg'
+
+      if 'iphone12' in name and 'mini' not in name:
+          return path_to_iphone + 'iphone/iphone_12.jpg'
+
+      if 'iphone11' in name and 'mini' not in name:
+          return path_to_iphone + 'iphone/iphone_11.jpg'
+
+      if 'iphonese' in name:
+          return path_to_iphone + 'iphone/iphone_se.PNG'
