adacdbd396592cdf4534ab74bb05af3fe69a952d
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-06-18 18:31:19 +0300

    Update views.py

--- tune_admin/views.py
+++ tune_admin/views.py
@@ -969,8 +969,19 @@ main_menu = [[buttons.name] for buttons in main_menu]
 main_menu.append(['⬅️Главное меню'])
 
 
-@client.message_handler(commands=['ti'])
 @client.message_handler(func=lambda message: message.text == 'Trade-in / Продажа')
+def trade_main(message, text='Выберите устройство'):
+    start_message(message,
+                  text='Программа trade-in доступна!\n'
+                       'С помощью нее вы можете сдать свое старое устройство'
+                       ' Apple и получить скидку на новое или б/у'
+                       ' (так же принятое по программе trade-in).\n'
+                       'Чтобы узнать размер скидки выберите пункт '
+                       '«Связаться с менеджером»\n'
+                       'Или позвоните по телефону: \n'
+                       '+7 (932) 222-54-45')
+
+@client.message_handler(commands=['/trad'])
 @client.message_handler(func=lambda message: message.text == '⬅️Назад к Trade-in')
 def trade_main(message, text='Выберите устройство'):
     list_user = TelegramUserModel.objects.all()
