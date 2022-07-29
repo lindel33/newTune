3b3a94c835c284889d380fb8ce4b796c3c73a7ec
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-07-29 11:03:56 +0300

    Update admin.py

--- provider/admin.py
+++ provider/admin.py
@@ -17,3 +17,47 @@ class ProviderProductAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         ss = ProviderProduct.objects.filter(author=request.user)
         return ss
+    
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        self.readonly_fields = ['image_1',
+                       'image_2',
+                       'image_3',
+                       'price',
+                       'smile',
+                       'name',
+                       'tests',
+                       'article',
+                       'state',
+                       'state_akb',
+                       'works',
+                       'kit',
+                       'guaranty',
+                       'custom_guaranty',
+                       'base_text',
+                       'regin',
+                       'category',
+                       'series',
+                      ]
+        self.fields = ['image_1',
+                       'image_2',
+                       'image_3',
+                       'sell',
+                       'booking',
+                       'price',
+                       'smile',
+                       'name',
+                       'tests',
+                       'article',
+                       'state',
+                       'state_akb',
+                       'works',
+                       'kit',
+                       'guaranty',
+                       'custom_guaranty',
+                       'base_text',
+                       'regin',
+                       'category',
+                       'series',
+                      ]
+
+        return self.changeform_view(request, object_id, form_url, extra_context)
